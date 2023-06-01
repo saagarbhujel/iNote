4707907aade5d1d376e9b8205e800a7290e2752e
saagarbhujel <saagarbhujel22@gmail.com>
2023-06-02 00:33:21 +0545

    navbar responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-
-import './App.css'
-import About from './components/About';
-import Contact from './components/Contact';
-import Home from './components/Home';
-import NavBar from './components/NavBar';
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
-
-
-
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
@@ -24,10 +16,8 @@ function App() {
           <Route exact path="/contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter>
-
-
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
+  const [mobileNav, setMobileNav] = useState(false)
+
+  const handdleHam = ()=>{
+    setMobileNav(!mobileNav)
+  }
+
   return (
     <>
-      <nav className=" bg-purple-900 text-white py-3 text-lg ">
-        <ul className=" flex justify-end px-4 mr-16">
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/about">
-            <li className=" mx-5">About</li>
-          </Link>
-          <Link to="/contact">
-            <li>Contact</li>
-          </Link>
-        </ul>
+      {/* FOR BIIGER DEVICES */}
+      <nav className=" flex justify-between h-15 mx-w-[1240px] mx-auto px-4 bg-purple-900 text-white py-3 text-lg relative z-10 ">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] ">iNote</h1>
+        <div className="nav-list">
+          <ul className="hidden md:flex justify-center px-4 mr-16 gap-[4vw] uppercase">
+            <Link to="/">
+              <li className="hover:underline">Home</li>
+            </Link>
+            <Link to="/about">
+              <li className="hover:underline ">About</li>
+            </Link>
+            <Link to="/contact">
+              <li className="hover:underline">Contact</li>
+            </Link>
+          </ul>
+        </div>
+
+        {/* FOR MOBILE */}
+
+        <div onClick={handdleHam} className="flex  items-center md:hidden">
+          {!mobileNav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        </div>
+        
+        <div className={!mobileNav ? 'nav-list fixed top-0 left-0 w-[60%] h-full mt-[60px] border-r border-r-purple-900  bg-purple-200  ease-linear duration-300 md:hidden ': 'fixed left-[-100%]'}>
+          <ul className=" text-black uppercase text-center ">
+            <Link to="/">
+              <li className="p-4  hover:underline">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="p-4 hover:underline ">
+                About
+              </li>
+            </Link>
+            <Link to="/contact">
+              <li className="p-4  hover:underline ">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
       </nav>
     </>
   );
