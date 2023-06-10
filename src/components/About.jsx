c3c769a443933c9d3f680ce8150a5bd094d677c0
saagarbhujel <saagarbhujel22@gmail.com>
2023-06-10 14:17:42 +0545

    unwanted code removed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,11 @@
-import React, { useContext, useEffect } from "react";
-import NoteContext from "../context/notes/NoteContext";
+import React from "react";
+
 
 const About = () => {
-  const a = useContext(NoteContext)
-  useEffect(()=>{
-    a.update()
-  },[])
+ 
   return (
     <>
-    <div>This is {a.state.name} and he is {a.state.age}</div>
+    <div>This is about</div>
 
     </>
   )
--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
+
 import NoteContext from "./NoteContext";
 
 const NoteState = (props)=>{
-    const s1 ={
-        "name" : "Ocean",
-        "age":"2"
-    }
-    const [state, setState] = useState(s1)
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-        "name" : "SAAGAR",
-        "age":"3"
-    })
-    }, 2000);
-    }
+  
     return(
         <>
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
         {props.children}
         </NoteContext.Provider>
         
