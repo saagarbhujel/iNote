776704d3c920cf7e9ec2a5e4f9a6c47bd492d8c7
saagarbhujel <saagarbhujel22@gmail.com>
2023-06-10 14:57:45 +0545

    form added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className="container flex justify-center items-center flex-col mt-6">
+      <h1 className="text-[2rem]">Add a Note</h1>
+      <form action="submit" className="max-w-md mx-auto">
+        <div className="mb-4">
+          <label htmlFor="title" className="form-label block mb-2">
+            Title:
+          </label>
+          <input
+            className="w-full border border-gray-400 rounded py-2 px-4 focus:outline-none focus:border-blue-500"
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2" htmlFor="message">
+            Message:
+          </label>
+          <textarea
+            className="w-full border border-gray-400 rounded py-2 px-4 h-32 resize-none focus:outline-none focus:border-blue-500"
+            id="message"
+            name="message"
+            required
+          />
+        </div>
+      </form>
+      <div className="button">
+        <button className="bg-purple-900 hover:bg-purple-600 text-white py-2 px-8 rounded">
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
