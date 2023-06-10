a77958a9d990846caf82f7cd574e64d0fb3f4967
saagarbhujel <saagarbhujel22@gmail.com>
2023-06-10 16:49:38 +0545

    fetching notes from context (hard code note)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import noteContext from "../context/notes/NoteContext"
 
 const Home = () => {
+
+  const context = useContext(noteContext)
+  const{notes, setNotes} = context
   return (
     <div className="container flex justify-center items-center flex-col mt-6">
-      <h1 className="text-[2rem]">Add a Note</h1>
+      <h1 className="text-[2rem] underline underline-offset-4">Add a Note</h1>
       <form action="submit" className="max-w-md mx-auto">
         <div className="mb-4">
           <label htmlFor="title" className="form-label block mb-2">
@@ -33,6 +37,12 @@ const Home = () => {
         <button className="bg-purple-900 hover:bg-purple-600 text-white py-2 px-8 rounded">
           Add
         </button>
+      </div>
+      <div className='mt-4'>
+        <h2 className="text-[1.8rem] underline underline-offset-4">Your Notes</h2>
+        {notes.map((note)=>{
+          return note.title
+        })}
       </div>
     </div>
   );
--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,11 +1,33 @@
-
+import { useState } from "react";
 import NoteContext from "./NoteContext";
 
+
 const NoteState = (props)=>{
+const notesInitial = [
+  {
+    _id: "64844146b37f51041d31b587",
+    user: "6473790dab9b1e7a06612e3e",
+    title: "First note",
+    description: "this is my first notes",
+    tag: "personal",
+    date: "2023-06-10T09:24:22.659Z",
+    __v: 0,
+  },
+  {
+    _id: "64845517dfcb383d47753b56",
+    user: "6473790dab9b1e7a06612e3e",
+    title: "Second note",
+    description: "this is my first notes",
+    tag: "personal",
+    date: "2023-06-10T10:48:55.414Z",
+    __v: 0,
+  },
+];
   
+const [notes, setNotes] = useState(notesInitial)
     return(
         <>
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
         {props.children}
         </NoteContext.Provider>
         
