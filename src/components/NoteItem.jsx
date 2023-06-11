a4617ff52f15f2e360a907c2544f7be823bcc55f
saagarbhujel <saagarbhujel22@gmail.com>
2023-06-11 22:09:09 +0545

    hover added in notes

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -6,7 +6,7 @@ const NoteItem = (props) => {
     <>
       {/* {note.title}
       {note.description} */}
-      <div className="card border rounded-lg shadow-md m-4 p-4  columns-md ">
+      <div className="card border rounded-lg shadow-md hover:shadow-lg m-4 p-4  columns-md ">
         <div className="card-body ">
           <div className="card-title font-bold text-xl mb-2">{note.title}</div>
           <div className="card-description text-gray-700">
