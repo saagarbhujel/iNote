8b605a61e5b2860e1257beb81600b10234987fcb
saagarbhujel <saagarbhujel22@gmail.com>
2023-07-04 22:54:33 +0545

    login signup path added

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
+import Signup from "./components/Signup";
+import Login from "./components/login";
 
 function App() {
   return (
     <>
-    <NoteState>
-
-   
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/contact" element={<Contact />}></Route>
-        </Routes>
-      </BrowserRouter>
-     </NoteState>
+      <NoteState>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
