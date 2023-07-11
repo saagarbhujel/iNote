f0a12697fd9a4a13fedab4bbdb5103554d5a29ae
saagarbhujel <saagarbhujel22@gmail.com>
2023-07-11 21:23:34 +0545

    alret contex and alertstate made

---
+++ src/context/alerts/AlertState.jsx
@@ -0,0 +1,11 @@
+import {AlertContext} from '../alerts/AlertContext';
+
+import React from 'react'
+
+const AlertState = () => {
+  return (
+    <div>AlertState</div>
+  )
+}
+
+export default AlertState
