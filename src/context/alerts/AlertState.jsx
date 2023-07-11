001cc5f191e54d6e98ae273d78b8e0cf9e877783
saagarbhujel <saagarbhujel22@gmail.com>
2023-07-12 03:15:15 +0545

    alert state is made

--- src/context/alerts/AlertState.jsx
+++ src/context/alerts/AlertState.jsx
@@ -1,10 +1,27 @@
-import {AlertContext} from '../alerts/AlertContext';
-
 import React from 'react'
+import { useState,useContext } from "react";
+import AlertContext from "./AlertContext";
 
-const AlertState = () => {
+const AlertState = (props) => {
+
+    const [alert, setAlert] = useState(null)
+
+    const showAlert = (message, type)=>{
+        setAlert({
+            msg:message,
+            type:type
+        });
+
+         setTimeout(() => {
+           setAlert(null);
+         }, 1500);
+    }
   return (
-    <div>AlertState</div>
+    <>
+    <AlertContext.Provider value={{alert,showAlert}} >
+        {props.children}
+    </AlertContext.Provider>
+    </>
   )
 }
 
