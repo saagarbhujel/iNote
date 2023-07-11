92530a63eeb8982a752f7e5b04432fa8c697ddaa
saagarbhujel <saagarbhujel22@gmail.com>
2023-07-12 03:13:56 +0545

    alert imported

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
 import Signup from "./components/Signup";
 import Login from "./components/login";
+import AlertState from "./context/alerts/AlertState";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <>
       <NoteState>
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route exact path="/contact" element={<Contact />}></Route>
-            <Route exact path="/login" element={<Login />}></Route>
-            <Route exact path="/signup" element={<Signup />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <AlertState>
+          <BrowserRouter>
+            <NavBar />
+            <Alert/>
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+              <Route exact path="/login" element={<Login />}></Route>
+              <Route exact path="/signup" element={<Signup />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </AlertState>
       </NoteState>
     </>
   );
