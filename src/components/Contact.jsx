79718c09753dad3a147d04af9811a7cebab0abfd
saagarbhujel <saagarbhujel22@gmail.com>
2023-07-18 15:45:15 +0545

    btn color and font size changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
             <button
               type="submit"
               disabled
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-purple-900 hover:bg-purple-600 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Submit
             </button>
